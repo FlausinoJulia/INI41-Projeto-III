4f02e0555f9ab2beb423ba59a39c3e65a476982e
DANYELLE NOGUEIRA FRANCA <u21232@academico.cotuca.unicamp.br>
2022-11-08 11:41:05 -0300

    layout

--- app/src/main/java/br/unicamp/cidadesmarte/MainActivity.java
+++ app/src/main/java/br/unicamp/cidadesmarte/MainActivity.java
@@ -3,6 +3,8 @@ package br.unicamp.cidadesmarte;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.cidades_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
     }
 }
