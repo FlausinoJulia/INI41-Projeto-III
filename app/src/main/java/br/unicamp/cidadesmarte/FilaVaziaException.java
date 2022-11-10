ad8f3ff0e71f2d299a8c9bdf8666ed0cd71124c1
DANYELLE NOGUEIRA FRANCA <u21232@academico.cotuca.unicamp.br>
2022-11-10 12:34:55 -0300

    exception

--- app/src/main/java/br/unicamp/cidadesmarte/FilaVaziaException.java
+++ app/src/main/java/br/unicamp/cidadesmarte/FilaVaziaException.java
@@ -1,6 +1,6 @@
 package br.unicamp.cidadesmarte;
 
-public class FilaVaziaException {
+public class FilaVaziaException extends Exception{
     public FilaVaziaException(String errorMessage) {
         super(errorMessage);
     }
